f2a0135ea54d4945e933b4eba23e4a1ee3cfdd35
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-08 22:09:43 +0530

    Update app.py

--- Task1_Google_Gemini_Chatbot/app.py
+++ Task1_Google_Gemini_Chatbot/app.py
@@ -1,3 +1,32 @@
 
 
-print("Welcome to Gemini AI Chatbot")
+#print("Welcome to Gemini AI Chatbot")
+import streamlit as st
+from chatbot import get_response
+
+st.set_page_config(
+    page_title="Gemini AI Chatbot",
+    page_icon="🤖",
+)
+
+st.title("🤖 Gemini AI Chatbot")
+
+user_input = st.text_area(
+    "Ask me anything..."
+)
+
+if st.button("Generate Response"):
+
+    if user_input.strip():
+
+        with st.spinner("Thinking..."):
+
+            answer = get_response(user_input)
+
+        st.success("Response")
+
+        st.write(answer)
+
+    else:
+
+        st.warning("Please enter a prompt.")
