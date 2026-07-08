9cb8055d63b98db2a1de3633882f4e86dbc379d1
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-09 02:28:25 +0530

    Update utils.py

--- Task1_Google_Gemini_Chatbot/utils.py
+++ Task1_Google_Gemini_Chatbot/utils.py
@@ -6,3 +6,20 @@ def get_timestamp():
     """
 
     return datetime.now().strftime("%d-%m-%Y %I:%M:%S %p")
+
+
+def export_chat(messages):
+
+    text=""
+
+    for msg in messages:
+
+        text += f"[{msg['time']}]\n"
+
+        text += f"{msg['role'].upper()}:\n"
+
+        text += msg["content"]
+
+        text += "\n\n"
+
+    return text
