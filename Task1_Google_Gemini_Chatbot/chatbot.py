945913283f5981368f5b30e64e7a52069c2b5f56
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-09 00:55:02 +0530

    Update chatbot.py

--- Task1_Google_Gemini_Chatbot/chatbot.py
+++ Task1_Google_Gemini_Chatbot/chatbot.py
@@ -4,7 +4,9 @@ from config import GEMINI_API_KEY
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 
-def get_response(prompt: str) -> str:
+def get_response(history):
+#def get_response(prompt: str) -> str:
+
     """
     Sends a prompt to Gemini and returns the response.
     """
