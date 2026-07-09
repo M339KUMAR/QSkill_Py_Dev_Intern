5a21c48f0e9c2b0bdc868b3f6c1cc4c526eea69c
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-09 17:15:05 +0530

    Update config.py

--- Task1_Google_Gemini_Chatbot/config.py
+++ Task1_Google_Gemini_Chatbot/config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+#GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not GEMINI_API_KEY:
     raise ValueError(
