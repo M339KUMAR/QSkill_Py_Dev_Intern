24778fa07dc4cacd85de186c201724f4270404b3
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-09 18:47:35 +0530

    Update database.py

--- Task1_Google_Gemini_Chatbot/database.py
+++ Task1_Google_Gemini_Chatbot/database.py
@@ -4,3 +4,41 @@ DB_NAME = "database/chatbot.db"
 
 def get_connection():
     return sqlite3.connect(DB_NAME)
+
+def initialize_database():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS conversations(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            title TEXT,
+
+            created_at TEXT
+
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            conversation_id INTEGER,
+
+            role TEXT,
+
+            content TEXT,
+
+            timestamp TEXT
+
+        )
+    """)
+
+    conn.commit()
+
+    conn.close()
